20fc018901cbc42b1de91a316e78a7f4ed0da5e7
Philemon Selvaraj <phil.lloyds@gmail.com>
2015-07-11 20:29:51 +0000

    Add Columns in the Users Table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true
+  validates :full_name,presence: true
+  validates :user_type,presence: true
+  validates :user_score, numericality: true
+  
 end
---
+++ db/migrate/20150711202922_adduser_name_to_users.rb
@@ -0,0 +1,4 @@
+class AdduserNameToUsers < ActiveRecord::Migration
+  def change
+  end
+end
