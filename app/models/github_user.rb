3214a47e71695e9a6d46d0024c4570b63e39e959
Philemon Selvaraj <phil.lloyds@gmail.com>
2015-07-11 20:37:40 +0000

    Add associations for users and github_users table

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -1,3 +1,4 @@
 class GithubUser < ActiveRecord::Base
   belongs_to :user
+  has_many :github_repo 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   validates :full_name,presence: true
   validates :user_type,presence: true
   validates :user_score, numericality: true
-  
+  validates :user_name,uniqueness: true
+  has_many :statistic
 end
