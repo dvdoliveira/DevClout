5954dcdcd1eb8e1059a1a57613d98ece58eb573c
Deivide (David) Oliveira <deivide.s.oliveira@gmail.com>
2015-07-12 19:29:23 +0000

    Change auth methods and organize secret keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'],scope: "user,repo"
+  provider :github, Rails.application.secrets.omniauth_github_key, Rails.application.secrets.omniauth_github_secret, scope: "user,repo"
 end
