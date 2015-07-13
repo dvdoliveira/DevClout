0bd2b30a1a0ea8c156927ebf0dd334d1300ebe89
Deivide (David) Oliveira <deivide.s.oliveira@gmail.com>
2015-07-13 19:57:34 +0000

    Add StackExchange authentication keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :github, Rails.application.secrets.omniauth_github_key, Rails.application.secrets.omniauth_github_secret, scope: "user,repo"
+  provider :github, 
+    Rails.application.secrets.omniauth_github_key, 
+    Rails.application.secrets.omniauth_github_secret, 
+    scope: "user,repo"
+  provider :stackexchange, 
+    Rails.application.secrets.omniauth_stackexchange_client_id, 
+    Rails.application.secrets.omniauth_stackexchange_client_secret, 
+    public_key: Rails.application.secrets.omniauth_stackexchange_public_key, 
+    site: 'stackoverflow'
 end
