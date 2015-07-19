480214618cfeb25fde95d8acadf1e9726da12429
Brian Li <brian.kasing.li@gmail.com>
2015-07-19 03:38:32 +0000

    cleaned up the controller update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,14 +26,6 @@ class UsersController < ApplicationController
         format.json { render json: @thing.errors.full_messages, status: :unprocessable_entity }
       end
     end
-    # respond_to do |format|
-    #   @user = User.find(params[:id])
-    #   if @user.update(user_params)
-    #     format.html { redirect_to profile_path, notice: 'User was successfully updated.' }
-    #   else
-    #     format.html { render action: "edit" }
-    #   end
-    # end
   end
 
   def logout
