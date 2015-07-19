ee26c952d385352070f030fe2a8389451522167b
Deivide (David) Oliveira <deivide.s.oliveira@gmail.com>
2015-07-19 01:59:02 +0000

    Change twitter credentials on twitter initializer

--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,8 +1,8 @@
 require 'twitter'
 
-Twitter.configure do |config|
+$twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = Rails.application.secrets.omniauth_twitter_key
   config.consumer_secret = Rails.application.secrets.omniauth_twitter_secret
-  config.oauth_token = Rails.application.secrets.twitter_access_token
-  config.oauth_token_secret = Rails.application.secrets.twitter_access_token_secret
+  config.access_token = Rails.application.secrets.twitter_access_token
+  config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
 end
