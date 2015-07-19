f179c5904b6b2b7e64686e2ea3e71c2cafbd5f06
Deivide (David) Oliveira <deivide.s.oliveira@gmail.com>
2015-07-19 22:42:42 +0000

    Update twitter data using user access_tokens instead of our application access token to avoid rate limits

--- app/interactors/create_twitter_user.rb
+++ app/interactors/create_twitter_user.rb
@@ -1,5 +1,17 @@
 class CreateTwitterUser
   include Interactor
+  require 'twitter'
+
+  def connect_twitter(twitter_user)
+    $twitter = Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.secrets.omniauth_twitter_key
+      config.consumer_secret = Rails.application.secrets.omniauth_twitter_secret
+      config.access_token = twitter_user.access_token
+      config.access_token_secret = twitter_user.secret
+      # config.access_token = Rails.application.secrets.twitter_access_token
+      # config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+    end
+  end
 
   def call
     @twitter_user = TwitterUser.create(
@@ -27,6 +39,7 @@ class CreateTwitterUser
       statuses_count: context.auth[:extra][:raw_info][:statuses_count]
     )
     update_user_twitter_id(@twitter_user)
+    connect_twitter(@twitter_user)
     save_twitter_followers_to_relationships(@twitter_user)
     save_twitter_following_to_relationships(@twitter_user)
   end
